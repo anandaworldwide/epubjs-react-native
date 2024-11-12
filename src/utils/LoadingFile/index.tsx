eae5ff154804b1e244c4d97fe33cc885fedfd666
Nabha Cosley <nabhacosley@gmail.com>
2024-11-11 19:35:36 -0800

    Remove 'Loading...' text when book loads

--- src/utils/LoadingFile/index.tsx
+++ src/utils/LoadingFile/index.tsx
@@ -12,8 +12,6 @@ export function LoadingFile({
   return (
     <View style={[styles.container, { width, height }]}>
       <ActivityIndicator size="large" />
-
-      <Text style={styles.text}>Loading {downloadProgress}%</Text>
     </View>
   );
 }
